0f33eb88cda7e322756dbd98b4ff55fb8c37a5eb
azlo-aj <github@nook.email>
2023-06-21 20:51:49 -0500

    Update splash page to vaporwave style

---
+++ code.js
@@ -0,0 +1,33 @@
+
+
+function buttonUp(button) {
+    button.style.webkitFilter = "brightness(100%)";
+    button.style.borderBottom = "2px solid black";
+    button.style.borderRight = "2px solid black";
+    button.style.borderTop = "2px solid white";
+    button.style.borderLeft = "2px solid white";
+}
+function buttonDown(button) {
+    button.style.webkitFilter = "brightness(110%)";
+    button.style.borderBottom = "2px solid white";
+    button.style.borderRight = "2px solid white";
+    button.style.borderTop = "2px solid black";
+    button.style.borderLeft = "2px solid black";
+}
+
+// window hiding 
+let minimize = document.querySelector('#min');
+let windowOpen = true;
+function windowResize() {
+    if (windowOpen === true) {
+        document.querySelector('#border').style.opacity = "0";
+        document.querySelector('#border').style.transform = "scale(1, 0) ";
+        windowOpen = false;
+    } else {
+        document.getElementById('border').style.opacity = "100";
+        document.querySelector('#border').style.transform = "scale(1, 1)";
+        windowOpen = true;
+    }
+}
+minimize.addEventListener('click', windowResize);
+
